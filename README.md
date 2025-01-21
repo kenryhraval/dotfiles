007b779c79db138201d99de216127f5511287626
Henrijs Kravals <henrijskravals@gmail.com>
2025-01-21 14:53:13 +0200

    Add bash script for resolving dotfile conflicts and setting up symlinks in a fresh environment.

---
+++ README.md
@@ -0,0 +1,22 @@
+Dotfile repository
+
+==================
+
+This repository contains a collection of dotfiles for setting up a personalized environment on a Unix-like system. It provides configurations for various tools, such as the shell, editors, and other utilities, that can be symlinked to your home directory.
+
+# Installation
+	
+Copy the `dotfile` directory to your `home` directory:
+
+```bash
+git clone https://github.com/kenryhraval/dotfiles.git ~/dotfiles
+```
+
+# Setup
+
+Go to the freshly created `~/dotfiles` directory and execute the resolver that copies the already existing unique dotfiles to the `dotfiles` directory and discards the ones alrady found in the `dotfiles` directory (backup by uncommenting a line in the file `resolver.sh`), after that creates symlinks in the `home` directory for all dotfiles. 
+
+```bash
+cd ~/dotfiles
+source resolver.sh
+```
---
+++ resolver.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# 1) overrides already existing dotfiles in the TARGET_DIR if they are within the DOTFILES_DIR (can be backuped by uncommenting line 26) 
+# 2) moves all dotfiles within the TARGET_DIR to the DOTFILES_DIR and creates symlinks within the TARGET_DIR
+
+DOTFILES_DIR=~/dotfiles
+
+# (your home directory)
+TARGET_DIR=~
+
+# move already existing unique dotfiles 
+for file in "$TARGET_DIR"/.*; do
+
+    basefile=$(basename "$file")
+
+    # skip unwanted files: . (current), .. (parent), the dotfiles directory, symlinks, or existing files in the dotfiles directory
+    if [[ "$file" == "$TARGET_DIR/." || "$file" == "$TARGET_DIR/.." || "$file" == "$DOTFILES_DIR" || -L "$file" || -e "$DOTFILES_DIR/$basefile" ]]; then
+        continue
+    fi
+
+    # ensure the file is a regular file (not a directory)
+    if [ -f "$file" ]; then
+        mv "$file" "$DOTFILES_DIR/$basefile"
+    fi
+done
+
+
+for file in "$DOTFILES_DIR"/.*; do
+
+  # we don't want symlinks for these
+  if [[ "$file" == "$DOTFILES_DIR/." || "$file" == "$DOTFILES_DIR/.." ]]; then
+    continue
+  fi
+  
+  basefile=$(basename "$file")
+  
+  target="$TARGET_DIR/$basefile"
+
+  # if the dotfile file already exists 
+  if [ -e "$target" ] || [ -L "$target" ]; then
+    # you may want to backup, in which case uncomment this
+    # mv "$target" "$target.bak"
+    true	  
+  fi
+
+  # create the symlink
+  ln -s "$file" "$target"
+done
+
+echo "symlinking completed"
+
